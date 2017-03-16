0fb994fdd724b3a8cc09ed28f0240cb0e3954e7c
Alejandro Barros Cuetos <jandrob1978@gmail.com>
2017-03-16 17:37:14 +0000

    Example and library code

---
+++ Example/Pods/Target Support Files/ConnectivityIndicator/ConnectivityIndicator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ConnectivityIndicator : NSObject
+@end
+@implementation PodsDummy_ConnectivityIndicator
+@end
---
+++ Example/Pods/Target Support Files/Pods-ConnectivityIndicator_Example/Pods-ConnectivityIndicator_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ConnectivityIndicator_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ConnectivityIndicator_Example
+@end
